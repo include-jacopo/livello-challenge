7fadeec1224e85d7c7af95fcef9448081c9d53cb
include-jacopo <jacopopati@gmail.com>
2022-02-02 11:36:46 +0100

    Movies list styles

--- pages/home/_Home.jsx
+++ pages/home/_Home.jsx
@@ -1,11 +1,15 @@
-import { Button } from 'react-native';
+import { Button, TextInput, TouchableOpacity } from 'react-native';
 import { StyleSheet, Text, View } from 'react-native';
 
 export default function Home({ navigation }) {
   return (
     <View style={styles.container}>
-      <Text>Home</Text>
-      <Button title="Go to search" onPress={() => navigation.navigate('search')} />
+      <View style={styles.search_cont}>
+        <TextInput placeholder="Search movies ..." style={styles.search_input} />
+      </View>
+      <View style={styles.list_cont}>
+          {[1,2,3,4,5].map((item, i) => <TouchableOpacity key={i} style={styles.list_item} onPress={() => navigation.navigate('search')}><Text>List item</Text></TouchableOpacity>)}
+      </View>
     </View>
   );
 }
@@ -15,7 +19,33 @@ const styles = StyleSheet.create({
     flex: 1,
     width: '100%',
     backgroundColor: '#fff',
-
+  },
+  search_cont: {
+    width: '100%',
+    alignItems: 'center',
+    marginTop: 20,
+  },
+  search_input: {
+    height: 50,
+    width: '90%',
+    borderWidth: 1,
+    padding: 5,
+    borderRadius: 10,
+  },
+  list_cont: {
+    flex: 1,
+    alignItems: 'center',
+    marginTop: 20,
+  },
+  list_item: {
+    marginTop: 10,
+    width: '90%',
+    height: 50,
+    backgroundColor: '#eee',
+    alignItems: 'flex-start',
+    justifyContent: 'center',
+    borderRadius: 10,
+    paddingLeft: 20,
   },
 });
 
